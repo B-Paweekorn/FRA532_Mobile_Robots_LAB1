43412750f9f5c73cf3983e6a13e0f467b55f7b54
chawrewerwee <paweekorn.pb@gmail.com>
2024-02-07 01:08:58 +0700

    planner structure

--- src/robot_control/scripts/global_planner.py
+++ src/robot_control/scripts/global_planner.py
@@ -14,56 +14,58 @@ class GlobalCostmapNode(Node):
 
         self.create_timer(0.05, self.update)
         
+        self.visualize = True
         self.map_received = False
         self.map_data = None
         self.map_info = None
-
+        
+        # robot pose [m]
         self.robot_x = 0.0
         self.robot_y = 0.0
 
+        # robot goal [m]
         self.goal_x = 0.0
         self.goal_y = 0.0
-
         
+        # robot properties [m]
+        self.robot_radius = 5.0
+
     def map_callback(self, msg):
         # Process the received map
         self.map_info = msg.info
         self.map_data = msg.data
 
     def update(self):
-        if self.map_data is not None and not self.map_received:
-            self.map_received = True
-            print("Received map !!!")
-            self.visualize_map()
-        else:
-            print("Waiting map...")
-    def visualize_map(self):
+        self.map, self.ox, self.oy = self.create_map()
+        if self.map_received is True:
+            if self.visualize is True:
+                print("Start visualizing map")
+                plt.imshow(self.map, cmap='gray', extent=[self.ox[0], self.ox[-1], self.oy[0], self.oy[-1]])
+                plt.colorbar()
+                plt.title('Occupancy Grid Map')
+                plt.xlabel('X Coordinate (m)')
+                plt.ylabel('Y Coordinate (m)')
+                plt.show()
+            
+    def create_map(self):
         if self.map_info is None:
-            self.get_logger().warn('No map info received.')
-            return
-
-        # Extract width and height from map info
+            self.get_logger().warn('Waiting for map.')
+            self.map_received = False
+            return None, None, None
+        print("Received map")
         width = self.map_info.width
         height = self.map_info.height
-        
-        # Convert map data to numpy array
-        map_array = np.array(self.map_data).reshape(height, width)
-        
-        # Extract origin position from map info
+        resolution = self.map_info.resolution
         origin_x = self.map_info.origin.position.x
         origin_y = self.map_info.origin.position.y
+
+        map_array = np.array(self.map_data).reshape(height, width)
+        map_array_x = np.linspace(origin_x, origin_x + resolution * width, width)
+        map_array_y = np.linspace(origin_y, origin_y + resolution * height, height)
         
-        # Create x and y coordinates for the map grid
-        x_coords = np.linspace(origin_x, origin_x + self.map_info.resolution * width, width)
-        y_coords = np.linspace(origin_y, origin_y + self.map_info.resolution * height, height)
-        
-        # Plot the map
-        plt.imshow(map_array, cmap='gray', extent=[x_coords[0], x_coords[-1], y_coords[0], y_coords[-1]])
-        plt.colorbar()
-        plt.title('Occupancy Grid Map')
-        plt.xlabel('X Coordinate (m)')
-        plt.ylabel('Y Coordinate (m)')
-        plt.show()
+        self.map_received = True
+        return map_array, map_array_x, map_array_y
+
 
 def main(args=None):
     rclpy.init(args=args)
