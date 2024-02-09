8811d96b3e3dea2a63e5ccc467e39524a475879a
chawrewerwee <paweekorn.pb@gmail.com>
2024-02-09 09:45:08 +0700

    algorithms

--- src/robot_control/scripts/global_planner.py
+++ src/robot_control/scripts/global_planner.py
@@ -8,6 +8,7 @@ from geometry_msgs.msg import PoseStamped
 import tf_transformations
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import deque
 
 class GlobalCostmapNode(Node):
     def __init__(self):
@@ -27,8 +28,8 @@ class GlobalCostmapNode(Node):
         self.robot_th = 0.0
 
         # robot goal [m]
-        self.goal_x = 0.0
-        self.goal_y = 0.0
+        self.goal_x = -5.0
+        self.goal_y = 5.0
         
         # robot properties [m]
         self.robot_radius = 5.0
@@ -44,16 +45,30 @@ class GlobalCostmapNode(Node):
         self.map_data = msg.data
 
     def update(self):
-        self.map, self.ox, self.oy = self.create_map()
+        self.map, self.scale_x, self.scale_y = self.create_map()
         if self.map_received is True:
-            self.vff_planner()
-            if self.visualize is True:
+            print("Received map")
+            self.vff_planner(self.robot_x, self.robot_y, self.goal_x, self.goal_y, self.obs_x, self.obs_y, 0.5, 0.40)
+            if self.visualize:
                 print("Start visualizing map")
-                plt.imshow(self.map, cmap='gray', extent=[self.ox[0], self.ox[-1], self.oy[0], self.oy[-1]])
-                plt.colorbar()
-                plt.title('Occupancy Grid Map')
-                plt.xlabel('X Coordinate (m)')
-                plt.ylabel('Y Coordinate (m)')
+                fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+                ax1.imshow(self.map, cmap='gray', extent=[self.scale_x[0], self.scale_x[-1], self.scale_y[0], self.scale_y[-1]])
+                ax1.set_title('Occupancy Grid Map')
+                ax1.set_xlabel('X Coordinate (m)')
+                ax1.set_ylabel('Y Coordinate (m)')
+                ax1.grid(True)
+
+                ax2.scatter(self.obs_x, self.obs_y, color='blue', marker='.')
+                ax2.set_title('Obstacle Positions')
+                ax2.set_xlabel('X Coordinate (m)')
+                ax2.set_ylabel('Y Coordinate (m)')
+                ax2.grid(True)
+
+                ax2.set_xlim(ax1.get_xlim())
+                ax2.set_ylim(ax1.get_ylim())
+
+                plt.tight_layout()
                 plt.show()
 
     def create_map(self):
@@ -61,27 +76,153 @@ class GlobalCostmapNode(Node):
             self.get_logger().warn('Waiting for map.')
             self.map_received = False
             return None, None, None
-        print("Received map")
-        width = self.map_info.width
-        height = self.map_info.height
-        resolution = self.map_info.resolution
-        origin_x = self.map_info.origin.position.x
-        origin_y = self.map_info.origin.position.y
+        
+        self.width = self.map_info.width
+        self.height = self.map_info.height
+        self.resolution = self.map_info.resolution
+        self.origin_x = self.map_info.origin.position.x
+        self.origin_y = self.map_info.origin.position.y
 
-        map_array = np.array(self.map_data).reshape(height, width)
+        map_array = np.array(self.map_data).reshape(self.height, self.width)
         
         filtercost_map = np.where(map_array < 100, 0, map_array)
-        filtercost_map = np.flipud(filtercost_map)
 
-        map_array_x = np.linspace(origin_x, origin_x + resolution * width, width)
-        map_array_y = np.linspace(origin_y, origin_y + resolution * height, height)
+        map_scale_x = np.linspace(self.origin_x, self.origin_x + self.resolution * self.width, self.width)
+        map_scale_y = np.linspace(self.origin_y, self.origin_y + self.resolution * self.height, self.height)
         
+        map_obstacle_x = []
+        map_obstacle_y = []
+
+        for i in range(self.height):
+            for j in range(self.width):
+                if filtercost_map[i][j] >= 100:
+                    map_obstacle_x.append(map_scale_x[j])
+                    map_obstacle_y.append(map_scale_y[i])
+
+        filtercost_map = np.flipud(filtercost_map)
+        self.obs_x = map_obstacle_x
+        self.obs_y = map_obstacle_y
         self.map_received = True
-        return filtercost_map, map_array_x, map_array_y
+        return filtercost_map, map_scale_x, map_scale_y
 
-    def vff_planner(self):
-        pass
+    def vff_planner(self, sx, sy, gx, gy, ox, oy, reso, rbtr):
+        # [sx, sy] : start point x, y
+        # [gx, gy] : goal point x, y
+        # [ox, oy] : obstacle x, y
+        # rese : resolution
+        # rbtr : robot radius
+        # calc potential field
+        print("Calculated potential map")
+        pmap, minx, miny = self.vff_calc(gx, gy, ox, oy, reso, rbtr)
+        print("Calculated potential map success")
+        # search path
+        d = np.hypot(sx - gx, sy - gy)
+        ix = round((sx - minx) / reso)
+        iy = round((sy - miny) / reso)
+        gix = round((gx - minx) / reso)
+        giy = round((gy - miny) / reso)
 
+        if self.visualize:
+            print("VFF started visualized")
+            self.draw_heatmap(pmap)
+            plt.gcf().canvas.mpl_connect('key_release_event',
+                    lambda event: [exit(0) if event.key == 'escape' else None])
+            plt.plot(ix, iy, "*k")
+            plt.plot(gix, giy, "*m")
+        
+        rx, ry = [sx], [sy]
+        motion = self.get_motion_model()
+
+        while d >= reso:
+            minp = float("inf")
+            minix, miniy = -1, -1
+            for i, _ in enumerate(motion):
+                inx = int(ix + motion[i][0])
+                iny = int(iy + motion[i][1])
+                if inx >= len(pmap) or iny >= len(pmap[0]) or inx < 0 or iny < 0:
+                    p = float("inf")  # outside area
+                    print("outside potential!")
+                else:
+                    p = pmap[inx][iny]
+                if minp > p:
+                    minp = p
+                    minix = inx
+                    miniy = iny
+            ix = minix
+            iy = miniy
+            xp = ix * reso + minx
+            yp = iy * reso + miny
+            d = np.hypot(gx - xp, gy - yp)
+            rx.append(xp)
+            ry.append(yp)
+
+            if self.visualize:
+                plt.plot(ix, iy, ".r")
+                plt.pause(0.01)
+
+        print("Goal!!")
+
+        return rx, ry
+    
+    def vff_calc(self, gx, gy, ox, oy, reso, rbtr):
+        minx = self.origin_x
+        miny = self.origin_y
+        maxx = self.origin_x + self.resolution * self.width
+        maxy = self.origin_y + self.resolution * self.height
+        xw = int(round((maxx - minx) / reso))
+        yw = int(round((maxy - miny) / reso))
+
+        pmap = [[0.0 for i in range(yw)] for i in range(xw)]
+
+        for ix in range(xw):
+            x = ix * reso + minx
+            print(ix*100/xw, "%")
+            for iy in range(yw):
+                y = iy * reso + miny
+                ug = self.calc_attractive_potential(x, y, gx, gy)
+                uo = self.calc_repulsive_potential(x, y, ox, oy, rbtr)
+                uf = ug + uo
+                pmap[ix][iy] = uf
+        return pmap, minx, miny
+
+    def calc_attractive_potential(self, x, y, gx, gy):
+        return 0.5 * 20 * np.hypot(x - gx, y - gy)
+
+    def calc_repulsive_potential(self, x, y, ox, oy, rbtr):
+        minid = -1
+        dmin = float("inf")
+        for i, _ in enumerate(ox):
+            d = np.hypot(x - ox[i], y - oy[i])
+            if dmin >= d:
+                dmin = d
+                minid = i
+
+        dq = np.hypot(x - ox[minid], y - oy[minid])
+
+        if dq <= rbtr:
+            if dq <= 0.1:
+                dq = 0.1
+            return 0.5 * 100.0 * (1.0 / dq - 1.0 / rbtr) ** 2
+        else:
+            return 0.0
+    
+    def draw_heatmap(self, data):
+        data = np.array(data).T
+        plt.pcolor(data, vmax=100.0, cmap=plt.cm.Blues)
+        
+    def get_motion_model(self):
+        # dx, dy
+        motion = [[1, 0],
+                [0, 1],
+                [-1, 0],
+                [0, -1],
+                [-1, -1],
+                [-1, 1],
+                [1, -1],
+                [1, 1]]
+        
+        return motion
+    
     def path_marker(self, path):
         for i in range(len(path)):
             # Publish the goal point for visualization in RViz
@@ -91,7 +232,6 @@ class GlobalCostmapNode(Node):
             self.goal_marker_publisher.publish(goal_marker)
             return path[i]
         return None
-        
 
 def main(args=None):
     rclpy.init(args=args)
